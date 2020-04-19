8af91b15f12efdb72bf5d3ae446f3ae78a2edeb9
Wreckeshey <RTSmith801@gmail.com>
2020-04-19 16:39:57 -0600

    Basic Movements and Animation
    Basic Movements and Animation

---
+++ DungeonCrawler/Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    public float moveSpeed = 5f;
+    Vector2 movement;
+
+
+    private void Start()
+    {
+        rb = transform.gameObject.GetComponent<Rigidbody2D>();
+        animator = transform.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Creating a dungeon crawler
 
 Art Assets imported from: Tiny RPG - Forest, by Ansimuz
+
+Developer notes:
+Animation Samples = 12
+Sprite Pixels Per Unit = 16
